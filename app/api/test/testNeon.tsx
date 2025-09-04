e3d0ac54d04d3d9abd373c4f7a58dcc90fcfcc11
lattesmoody <lattesmoody@gmail.com>
2025-09-04 15:28:00 +0900

    [DELETE,ADD] 1. 기존 ROUTE 테스트 페이지 삭제 2. testNeon 페이지 추가

---
+++ app/api/test/testNeon.tsx
@@ -0,0 +1,21 @@
+// File: app/page.tsx
+import { neon } from '@neondatabase/serverless';
+
+export default function Page() {
+  async function create(formData: FormData) {
+    'use server';
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+    // Insert the comment from the form into the Postgres database
+    // [MODIFY] 2025.09.04 "태그드 템플릿 함수는 템플릿 리터럴 문법인 백틱`으로 호출"
+    await sql`INSERT INTO comments (comment) VALUES (${comment})`;
+  }
+
+  return (
+    <form action={create}>
+      <input type="text" placeholder="write a comment" name="comment" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
