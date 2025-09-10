83d4e81d41bb4be0c0eabff0c65bf8923769e0e4
lattesmoody <lattesmoody@gmail.com>
2025-09-10 14:11:23 +0900

    [MODIFY] next.config.ts - 외부 접근 명시 허용 (휴대폰 핫스팟으로 테스트 페이지 접속 관련)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+/*
+  수정일자: 2025.09.10
+  작 성 자: SHIM
+  최근수정: "외부 접근 명시 허용."
+*/
+import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  // 'experimental' 대신 'devServer'를 사용
+  devServer: {
+    allowedDevOrigins: process.env.ALLOWED_DEV_ORIGIN
+      ? [process.env.ALLOWED_DEV_ORIGIN]
+      : [],
+  },
 };
 
-export default nextConfig;
+export default config;
