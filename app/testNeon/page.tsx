35e20d75a2878d33eb4cb7270317eb54d3d5190f
lattesmoody <lattesmoody@gmail.com>
2025-09-14 23:47:08 +0900

    [MODIFY] "테스트코드"

--- app/testNeon/page.tsx
+++ app/testNeon/page.tsx
@@ -1,15 +1,15 @@
 // File: app/page.tsx
 import { neon } from '@neondatabase/serverless';
+import { insertComment, NewCommentPayload } from '@/app/lib/sql/maps/commentQueries';
 
 export default function Page() {
   async function create(formData: FormData) {
     'use server';
     // Connect to the Neon database
     const sql = neon(`${process.env.DATABASE_URL}`);
-    const comment = formData.get('comment');
-    // Insert the comment from the form into the Postgres database
-    // [MODIFY] 2025.09.04 "태그드 템플릿 함수는 템플릿 리터럴 문법인 백틱`으로 호출"
-    await sql`INSERT INTO comments (comment) VALUES (${comment})`;
+    const comment = String(formData.get('comment') || '');
+    const payload: NewCommentPayload = { content: comment };
+    await insertComment(sql as any, payload);
   }
 
   return (
